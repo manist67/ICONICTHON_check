587b2a53386c7b835edd9fbf474faa41fd086a98
Kim Mani <manist67@naver.com>
2024-11-14 18:17:21 +0900

    🐛:bug: audio 등록 시 base64 prefix 제거

--- analyze_server/utils/base64_decode_audio.js
+++ analyze_server/utils/base64_decode_audio.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 function base64_decode_audio(base64Str, filename) {
   const ext = "mp3";
+  const base64Audio = base64Str.split(';base64,').pop();
   
   const pathname = path.resolve(__dirname, '../files', `${filename}.${ext}`);
 
-  fs.writeFileSync(pathname, base64Str, { encoding: 'base64' });
+  fs.writeFileSync(pathname, base64Audio, { encoding: 'base64' });
 }
 
 module.exports = base64_decode_audio
