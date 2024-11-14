4c1c1761cdc648a8b591af65f29bd9c1fb6b08cd
Kim Mani <manist67@naver.com>
2024-11-14 23:12:21 +0900

    🎨:art: mp3 -> wav 저장형태 변경

--- analyze_server/utils/base64_decode_audio.js
+++ analyze_server/utils/base64_decode_audio.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function base64_decode_audio(base64Str, filename) {
-  const ext = "mp3";
+  const ext = "wav";
   const base64Audio = base64Str.split(';base64,').pop();
   
   const pathname = path.resolve(__dirname, '../files', `${filename}.${ext}`);
